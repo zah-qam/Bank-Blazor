2cfe607abb79f0d217624e61bd57c7a7211296a0
zah-qam <zahqam.97@outlook.com>
2025-04-20 21:51:04 +0200

    Skapade mappen "Enums" och klassen "StatusCode.cs" innuti den. Skapade även "ICustomerService" inuuti mappen "Interfaces" med koden innuti den.

---
+++ BankBlazor.Api/Enums/StatusCode.cs
@@ -0,0 +1,15 @@
+﻿namespace BankBlazor.Api.Enums
+{
+    public enum ResponseCode
+    {
+        Success = 200,
+        Created = 201,
+        Accepted = 202,
+        NoContent = 204,
+        NotFound = 404,
+        BadRequest = 400,
+        Unauthorized = 401,
+        Forbidden = 403,
+        InternalServerError = 500
+    }
+}
---
+++ BankBlazor.Api/Services/Interfaces/ICustomerService.cs
@@ -0,0 +1,14 @@
+﻿using BankBlazor.Api.Data;
+using BankBlazor.Api.Enums;
+
+namespace BankBlazor.Api.Services.Interfaces
+{
+    public interface ICustomerService
+    {
+        Task<List<Customer>> GetAllCustomers();
+        Task<Customer?> GetCustomerById(int id); // frågetecken för nullable
+        Task<ResponseCode> CreateCustomer(Customer customer);
+        Task<ResponseCode> UpdateCustomer(Customer customer);
+        Task<ResponseCode> DeleteCustomer(int id);
+    }
+}
