cf8d4da53c392e92f68f3e321487e359ff3f8b34
zah-qam <zahqam.97@outlook.com>
2025-04-20 20:24:02 +0200

    Skapade kod innuti AccountReadDTO och AccountCreatDTO.

--- BankBlazor.Api/DTOs/AccountCreatDTO.cs
+++ BankBlazor.Api/DTOs/AccountCreatDTO.cs
@@ -1,6 +1,10 @@
 ﻿namespace BankBlazor.Api.DTOs
 {
-    public class AccountCreatDTO
+    public class AccountCreatDTO //Detta är det som kommer in i API:et, som användaren fyller i ett
+                                 //formulär, och som vi ska använda för att skapa en ny kund.
     {
+        public string Frequency { get; set; } = null!;
+        public DateOnly Created { get; set; }
+        public decimal Balance { get; set; }
     }
 }
--- BankBlazor.Api/DTOs/AccountReadDTO.cs
+++ BankBlazor.Api/DTOs/AccountReadDTO.cs
@@ -1,6 +1,11 @@
 ﻿namespace BankBlazor.Api.DTOs
 {
-    public class AccountReadDTO
+    public class AccountReadDTO // Det som kommer tillbaka till klienten, och som klienten kan se,
+                                // som kommer att visas i UI.
     {
+        public int AccountId { get; set; }
+        public string Frequency { get; set; } = null!;
+        public DateOnly Created { get; set; }
+        public decimal Balance { get; set; }
     }
 }
