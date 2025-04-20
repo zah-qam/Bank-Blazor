310defabdbe469e4f6f16e985c3c735da75177e8
zah-qam <zahqam.97@outlook.com>
2025-04-20 19:03:18 +0200

    Skapade CustomerReadDTO och koden innuti den.

--- BankBlazor.Api/DTOs/CustomerReadDTO.cs
+++ BankBlazor.Api/DTOs/CustomerReadDTO.cs
@@ -2,6 +2,14 @@
 {
     public class CustomerReadDTO
     {
-
+        public int CustomerId { get; set; }
+        public string Givenname { get; set; } = null!;
+        public string Surname { get; set; } = null!;
+        public string Streetaddress { get; set; } = null!;
+        public string City { get; set; } = null!;
+        public string Zipcode { get; set; } = null!;
+        public string Country { get; set; } = null!;
+        public string Emailaddress { get; set; }
+        public string? Telephonenumber { get; set; }
     }
 }
