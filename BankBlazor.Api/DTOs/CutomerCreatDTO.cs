decefed312dd97cbc5eee880cd3d40845d20ce9e
zah-qam <zahqam.97@outlook.com>
2025-04-20 19:23:08 +0200

    skapade en kommentar i CustomerReadDTO, och skapade klassen CustomerCreatDTO med koden innuti den.

--- BankBlazor.Api/DTOs/CutomerCreatDTO.cs
+++ BankBlazor.Api/DTOs/CutomerCreatDTO.cs
@@ -1,15 +1,16 @@
 ﻿namespace BankBlazor.Api.DTOs
 {
-    public class CustomerReadDTO // Det som kommer tillbaka till klienten, och som klienten kan se, som kommer att visas i UI.
+    public class CutomerCreatDTO //Detta är det som kommer in i API:et, som användaren fyller i ett formulär, och som vi ska använda för att skapa en ny kund.
     {
-        public int CustomerId { get; set; }
+        public string Gender { get; set; } = null!;
         public string Givenname { get; set; } = null!;
         public string Surname { get; set; } = null!;
         public string Streetaddress { get; set; } = null!;
         public string City { get; set; } = null!;
         public string Zipcode { get; set; } = null!;
         public string Country { get; set; } = null!;
-        public string Emailaddress { get; set; }
+        public string? Emailaddress { get; set; }
         public string? Telephonenumber { get; set; }
+        public DateOnly? Birthday { get; set; }
     }
 }
