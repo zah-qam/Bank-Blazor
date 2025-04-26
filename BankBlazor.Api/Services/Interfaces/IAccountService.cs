2941654f94947dbee7b1a747fa3ab73d2448ea6f
zah-qam <zahqam.97@outlook.com>
2025-04-26 23:25:48 +0200

    Skapade kod innuti IAccountService.cs

--- BankBlazor.Api/Services/Interfaces/IAccountService.cs
+++ BankBlazor.Api/Services/Interfaces/IAccountService.cs
@@ -1,6 +1,13 @@
-﻿namespace BankBlazor.Api.Services.Interfaces
+﻿using BankBlazor.Api.Data;
+
+namespace BankBlazor.Api.Services.Interfaces
 {
     public interface IAccountService
     {
+        Task<List<AccountReadDTO>> GetAccountByCustomerId(int customerId); // Ska göra en AccountReadDTO.
+        Task<decimal?> GetBalance(int accountId);
+        Task<Account> Transfer(int fromAccountId, int toAccountId, decimal amount);
+        Task<bool> Deposit(int accountId, decimal amount);
+        Task<bool> Withdraw(int accountId, decimal amount);
     }
 }
