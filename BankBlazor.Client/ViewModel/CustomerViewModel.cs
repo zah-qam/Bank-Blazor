b90f47585eb4d9a66850e950d28d0286a587bfff
zah-qam <zahqam.97@outlook.com>
2025-05-15 08:32:58 +0200

    Skapade CustomerReadViewModel. La till extra properties till CustomerViewModel.cs

--- BankBlazor.Client/ViewModel/CustomerViewModel.cs
+++ BankBlazor.Client/ViewModel/CustomerViewModel.cs
@@ -2,9 +2,14 @@
 {
     public class CustomerViewModel
     {
-        
+
         public int CustomerId { get; set; }
-        public string? CustomerName { get; set; }
+        public string Givenname { get; set; } = string.Empty;
+        public string Surname { get; set; } = string.Empty;
+        public string Emailaddress { get; set; } = string.Empty;
+        public string Streetaddress { get; set; } = string.Empty;
+        public string City { get; set; } = string.Empty;
+        public string Country { get; set; } = string.Empty;
         public List<AccountViewModel> Accounts { get; set; } = new();
     }
 
